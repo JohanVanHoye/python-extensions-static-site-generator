f62c9c95f129c098213186d65253a347843421c2
Johan Van Hoye <johan.vanhoye@wolterskluwer.com>
2022-05-12 22:20:41 +0200

    Module 3 - Fix 1 - isinstance method

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,5 +1,4 @@
-from ssg import hooks
-from ssg import parsers
+from ssg import hooks, parsers
 
 
 files = []
@@ -7,7 +6,7 @@ files = []
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: not(p.isinstance(parsers.ResourceParser))
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
